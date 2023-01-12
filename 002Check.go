cec23db59b6a4b83f1747ff9632a17aed2018556
John Nash <haikongyu@gmail.com>
2023-01-12 20:18:02 +0800

    添加保存匹配的功能像和结构像到文件的功能

--- 002Check.go
+++ 002Check.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -14,6 +15,17 @@ import (
 func CheckFile(regStrFun, regStrT1, root string) ([][]string, [][]string) {
 	regFun := regexp.MustCompile(regStrFun)
 	regT1 := regexp.MustCompile(regStrT1)
+	funFile, _ := os.OpenFile(filepath.Join(root, "FunImg.csv"), os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 644)
+	t1File, _ := os.OpenFile(filepath.Join(root, "T1Img.csv"), os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 644)
+	_, _ = funFile.Write([]byte{0xEF, 0xBB, 0xBF})
+	_, _ = t1File.Write([]byte{0xEF, 0xBB, 0xBF})
+	funCSVWriter := csv.NewWriter(funFile)
+	t1CSVWriter := csv.NewWriter(t1File)
+	defer func() {
+		_ = funFile.Close()
+		_ = t1File.Close()
+	}()
+
 	groups, _ := os.ReadDir(root)
 	var resultsFun [][]string
 	var resultsT1 [][]string
@@ -34,7 +46,11 @@ func CheckFile(regStrFun, regStrT1, root string) ([][]string, [][]string) {
 			if len(filesFunReg) != 1 || len(filesT1Reg) != 1 {
 				fmt.Println(fmt.Sprintf("Please check %v group %v subject Fun and T1 image", group.Name(), subject.Name()))
 			} else {
+				_ = funCSVWriter.Write([]string{root, group.Name(), subject.Name(), filesFunReg[0]})
+				funCSVWriter.Flush()
 				resultsFun = append(resultsFun, []string{root, group.Name(), subject.Name(), filesFunReg[0]})
+				_ = t1CSVWriter.Write([]string{root, group.Name(), subject.Name(), filesT1Reg[0]})
+				t1CSVWriter.Flush()
 				resultsT1 = append(resultsT1, []string{root, group.Name(), subject.Name(), filesT1Reg[0]})
 			}
 		}
