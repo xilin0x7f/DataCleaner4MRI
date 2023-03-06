bc404fac25b1a7e3b057d3e995946188c72eaceb
xilin0x7f <xilin0x7f@outlook.com>
2023-03-06 22:34:55 +0800

    add something for happy

--- 002Check.go
+++ 002Check.go
@@ -6,15 +6,26 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
+	"github.com/xuri/excelize/v2"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
 	"regexp"
+	"sort"
+	"strings"
 )
 
-func CheckFile(regStrFun, regStrT1, root string) ([][]string, [][]string) {
+func CheckFile(regStrFun, regStrT1, root, z string) ([][]string, [][]string) {
+	postN := 3
+	if z == "y" {
+		postN = 6
+	}
 	regFun := regexp.MustCompile(regStrFun)
 	regT1 := regexp.MustCompile(regStrT1)
+	var resultsFunJson, resultsT1Json []string
 	funFile, _ := os.OpenFile(filepath.Join(root, "FunImg.csv"), os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 644)
 	t1File, _ := os.OpenFile(filepath.Join(root, "T1Img.csv"), os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 644)
 	_, _ = funFile.Write([]byte{0xEF, 0xBB, 0xBF})
@@ -55,7 +66,88 @@ func CheckFile(regStrFun, regStrT1, root string) ([][]string, [][]string) {
 				t1CSVWriter.Flush()
 				resultsT1 = append(resultsT1, []string{root, group.Name(), subject.Name(), filesT1Reg[0]})
 			}
+			for funIdx := range filesFunReg {
+				resultsFunJson = append(resultsFunJson, filepath.Join(root, group.Name(), subject.Name(),
+					filesFunReg[funIdx][:len(filesFunReg[funIdx])-postN]+"json"))
+			}
+			for t1Idx := range filesT1Reg {
+				t1Origin := strings.Replace(filesT1Reg[t1Idx], "_Crop_1", "", -1)
+				resultsT1Json = append(resultsT1Json, filepath.Join(root, group.Name(), subject.Name(),
+					t1Origin[:len(t1Origin)-postN]+"json"))
+			}
+
 		}
 	}
+	if err := WriteJson2XLSX(resultsFunJson, filepath.Join(root, "Fun.xlsx"), "Sheet1", "A"); err != nil {
+		log.Fatal(err)
+	}
+	if err := WriteJson2XLSX(resultsT1Json, filepath.Join(root, "T1.xlsx"), "Sheet1", "A"); err != nil {
+		log.Fatal(err)
+	}
 	return resultsFun, resultsT1
+}
+
+func WriteJson2XLSX(filesName []string, dstFileName, sheetName, start string) error {
+	keysMap := make(map[string]int)
+	for _, fileName := range filesName {
+		file, _ := os.Open(fileName)
+		var jsonData map[string]interface{}
+		reader := io.Reader(file)
+		decoder := json.NewDecoder(reader)
+		_ = decoder.Decode(&jsonData)
+		_ = file.Close()
+		for key := range jsonData {
+			keysMap[key]++
+		}
+	}
+	var keys []string
+	for key := range keysMap {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	resMap := make(map[string][]interface{})
+	for _, fileName := range filesName {
+		file, _ := os.Open(fileName)
+		var jsonData map[string]interface{}
+		reader := io.Reader(file)
+		decoder := json.NewDecoder(reader)
+		_ = decoder.Decode(&jsonData)
+		_ = file.Close()
+		for _, key := range keys {
+			resMap[key] = append(resMap[key], jsonData[key])
+		}
+	}
+	res := make([][]interface{}, len(resMap[keys[0]])+1)
+	for idx := range res {
+		res[idx] = make([]interface{}, len(keys)+1)
+	}
+	res[0][0] = ""
+	for idx, key := range keys {
+		res[0][idx+1] = key
+	}
+	for rowIdx := range resMap[keys[0]] {
+		res[rowIdx+1][0] = filesName[rowIdx]
+		for colIdx := range keys {
+			res[rowIdx+1][colIdx+1] = resMap[keys[colIdx]][rowIdx]
+		}
+	}
+	err := Write2XLSX(dstFileName, sheetName, start, res)
+	return err
+}
+func Write2XLSX(fileName, sheetName, start string, data [][]interface{}) error {
+	f := excelize.NewFile()
+	index, err := f.NewSheet(sheetName)
+	if err != nil {
+		return err
+	}
+	f.SetActiveSheet(index)
+	for idx := range data {
+		if err := f.SetSheetRow(sheetName, fmt.Sprint(start, idx+1), &data[idx]); err != nil {
+			return err
+		}
+	}
+	if err := f.SaveAs(fileName); err != nil {
+		return err
+	}
+	return nil
 }
